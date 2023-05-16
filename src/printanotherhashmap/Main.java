4428fe493d55b6e2cf01ae288aa536f34259afca
Javier <d2j5@hotmail.com>
2023-05-16 19:58:26 -0400

    final

--- src/printanotherhashmap/Main.java
+++ src/printanotherhashmap/Main.java
@@ -6,16 +6,16 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-            // Create a new HashMap and add key-value pairs
-            HashMap<String, Book> hashmap = new HashMap<>();
-            hashmap.put("sense", new Book("Sense and Sensibility", 1811, "asd"));
-            hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "asdf"));
 
-            // Print all the values in the hashmap
+            HashMap<String, Book> hashmap = new HashMap<>();
+            hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
+            hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "..."));
+
+
             Program.printValues(hashmap);
             System.out.println("---");
 
-            // Print the books whose names contain the string "prejud"
+
         Program.printValueIfNameContains(hashmap, "prejud");
     }
 
--- src/printanotherhashmap/Program.java
+++ src/printanotherhashmap/Program.java
@@ -4,19 +4,14 @@ import java.util.HashMap;
 
 public class Program {
     public static void printValues(HashMap<String, Book> hashmap) {
-        // Iterate over the values in the hashmap
-        for (Book book : hashmap.values()) {
-            // Print the value using the toString method of the Book object
+            for (Book book : hashmap.values()) {
             System.out.println(book);
         }
     }
 
     public static void printValueIfNameContains(HashMap<String, Book> hashmap, String text) {
-        // Iterate over the values in the hashmap
         for (Book book : hashmap.values()) {
-            // Check if the name of the book contains the specified text
             if (book.getName().contains(text)) {
-                // Print the book
                 System.out.println(book);
             }
         }
